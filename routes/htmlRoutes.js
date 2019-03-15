dcfccc8026232ad5bc6bb0d4a90125b84f0c66ff
HaydnNeese <haydnneese@yahoo.com>
2019-03-14 21:38:03 -0500

    business manager page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,23 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.render("index");
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  app.get("/business", function(req, res) {
+    res.render("business_mngr");
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/user", function(req, res) {
+    res.render("user_main");
   });
 
   // Render 404 page for any unmatched routes
