4ee72cf63ff9875481de6c083ac39910fe108361
HaydnNeese <haydnneese@yahoo.com>
2019-03-16 18:42:36 -0500

    finished business page content and modal

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,10 +6,8 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
-
-  app.get("/", function(req, res) {
-
+module.exports = function (app) {
+  app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
@@ -17,21 +15,29 @@ module.exports = function(app) {
     res.render("index");
   });
 
-  app.get("/login", function(req, res) {
+  app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
       res.redirect("/members");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
-
   });
 
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", isAuthenticated, function(req, res) {
+  app.get("/members", isAuthenticated, function (req, res) {
     res.sendFile(path.join(__dirname, "../public/members.html"));
   });
+
+  app.get("/business", function (req, res) {
+    res.render("business_mngr");
+  });
+  app.get("/user", function (req, res) {
+    res.render("user");
+  });
 };
+
+
 
 // module.exports = function(app) {
 //   // Load index page
