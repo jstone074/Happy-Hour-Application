b92ed63d10dd8f6270278292948d8913be9530fa
mango dyl <dylanbonds@hotmail.com>
2019-03-19 18:23:34 -0500

    user populates with bars

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,18 +1,16 @@
-const express = require("express");
-const router = express.Router();
 var db = require("../models");
 
 module.exports = function(app) {
   // GET Bars
   app.get("/api/bars", function(req, res) {
     db.Business.findAll({}).then(data => {
-      console.log(data[0].dataValues);
+      console.log(data[0]);
       res.json(data);
     });
   });
 
   /* GET user profile. */
-  router.get("/profile", function(req, res, next) {
-    res.send(req.user);
-  });
+  // app.get("/profile", function(req, res, next) {
+  //   res.send(req.user);
+  // });
 };
