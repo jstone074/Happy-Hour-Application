6d3c5edeecc147a2c6297a920fd812723a929cad
Don Wasinger <DonWasinger@Dons-MacBook-Pro.local>
2019-03-21 09:59:56 -0500

    PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var path = require("path");
 var db = require("./models");
 
 var app = express();
-var PORT = process.env.JAWSDB_URL || 3000;
+var PORT = process.env.PORT || 3000;
 const auth = require("./routes/authRoutes");
 // const user = require("./routes/userRoutes");
 
